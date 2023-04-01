0c3aeb04c4dfb16bc12310530b3bd4e9b8950bb6
mt-martin <129513293+mt-usercontent@users.noreply.github.com>
2023-04-01 16:19:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["MTs'Blog", "Martin", "a blogger", "Monkey-K9"];
+const textArray = ["MTs' Blog", "Martin", "a blogger", "Monkey-K9"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
